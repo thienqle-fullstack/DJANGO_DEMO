cbd764ccda37e411f3075b919093e770558eb9a5
thienqle-fullstack <thienqle.vn@gmail.com>
2021-03-08 02:56:50 -0500

    add static

--- hello_project/settings.py
+++ hello_project/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 WSGI_APPLICATION = "hello_project.wsgi.application"
